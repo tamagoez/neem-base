5c385f99940e3a1582f6567de7892e6d89e27a13
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-07-30 16:30:23 +0000

    little update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 const IndexPage = () => (
   <>
-  <h1>Neem-Base</h1><p>Neemの中央サーバー的な役割を持っています。<br />Neem-Baseは、Neemのサーバー機能を持っていません!<br /><Link href="/serverlist">サーバー一覧</Link>からあなたのお好みのサーバーを探しましょう!</p>
+  <h1>NeemBase</h1><p>Neemの中央サーバー的な役割を持っています。<br />Neem-Baseは、Neemのサーバー機能を持っていません!<br /><Link href="/serverlist">サーバー一覧</Link>からあなたのお好みのサーバーを探しましょう!</p>
   </>
 )
 
--- pages/serverlist.tsx
+++ pages/serverlist.tsx
@@ -1,3 +1,16 @@
 export default function ServerList() {
-    return <><h1>サーバー一覧</h1><p>まだ何もサーバーがありません...</p></>
+  return (
+    <>
+      <h1>サーバー一覧</h1>
+      <p>
+        <a
+          href="https://neem.vercel.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          neem
+        </a>
+      </p>
+    </>
+  );
 }
