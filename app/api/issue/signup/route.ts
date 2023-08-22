eb5558bb7bc856142a5a019aca7d6c387ab66c27
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-22 17:27:33 +0000

    rapid fix

--- app/api/issue/signup/route.ts
+++ app/api/issue/signup/route.ts
@@ -1,9 +1,14 @@
 "use server";
 
 // Request Method
-// 
 //
-export default async function POST(request: Request) {
-  const res = await request.json()
+// apikey -> middleware.tsで捌く
+// secret => 確認用のsecret
+// token => 識別用のtoken
 
+import { NextResponse } from "next/server";
+
+export default async function POST(request: Request) {
+  const res = await request.json();
+  return NextResponse.json({});
 }
