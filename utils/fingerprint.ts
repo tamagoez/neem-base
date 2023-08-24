ab1b1089f0530e923fd4a35d3a3f36ea83f17ede
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-24 16:57:14 +0000

    delete fingerprint

--- utils/fingerprint.ts
+++
@@ -1,7 +0,0 @@
-import FingerprintJS from '@fingerprintjs/fingerprintjs'
-
-async function getVisitorId() {
-  const fp = await FingerprintJS.load()
-  const result = await fp.get()
-  return result.visitorId
-}
